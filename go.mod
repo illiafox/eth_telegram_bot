9ca53cf427e0d96fa4691ba407f61f8f52df197f
Illia <illiafox@github.com>
2022-02-12 12:51:04 +0200

    first working version

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/ethereum/go-ethereum v1.10.15 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"io/ioutil"
 	"log"
 	"math/big"
+	"net/url"
 	"regexp"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/shopspring/decimal"
 	"gopkg.in/yaml.v2"
 )
@@ -21,12 +24,14 @@ var (
 )
 
 type RPC struct {
-	name string `yaml:"name"`
-	path string `yaml:"path"`
+	Name   string `yaml:"name"`
+	Path   string `yaml:"path"`
+	Client *ethclient.Client
 }
 type Config_struct struct {
-	token string `yaml:"name"`  // Bot token
-	dials []RPC  `yaml:"dials"` // All ethereum connections
+	Token   string        `yaml:"token"`   // Bot token
+	Dials   []RPC         `yaml:"dials"`   // All ethereum connections
+	Timeout time.Duration `yaml:"timeout"` // Context timeout in millisecond
 }
 
 func main() {
@@ -36,26 +41,159 @@ func main() {
 	// read config file
 	data, err := ioutil.ReadFile(*config_path)
 	if err != nil {
-		log.Fatalln("Opening config:\n", err)
+		log.Fatalln("Opening config:\n\t", err)
 	}
 	// Main struct
-	var Config Config_struct
+	var Config = Config_struct{Timeout: time.Second}
 	// Parse yaml
 	err = yaml.Unmarshal(data, &Config)
 	if err != nil {
-		log.Fatalln("Parsing yaml:\n", err)
+		log.Fatalln("Parsing yaml:\n\t", err)
 	}
-	client, err := ethclient.Dial("https://www.ethercluster.com/etc")
-	if err != nil {
-		panic(err)
-	}
-	account := common.HexToAddress("0x78D5E220B4cc84f290Fae4148831b371a851a114")
-	balance, err := client.BalanceAt(context.Background(), account, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(balance)
-	d := decimal.NewFromBigInt(balance, 1)
-	fmt.Println(d.Div(div_wei))
+	// Update timeout
+	Config.Timeout = time.Millisecond * Config.Timeout
+	// Create context
+	ctx := context.Background()
+	// Check all rpc's
+	for i, rpc := range Config.Dials {
+		// Dial rpc client
+		rpc.Client, err = ethclient.Dial(rpc.Path)
+		if err != nil {
+			log.Fatalln("Dialing with:", rpc.Path, "\n\t", err)
 
+		}
+		// Ping client by calling last block number (TODO: Find better alternative)
+		_, err = rpc.Client.BlockNumber(ctx)
+		if err != nil {
+			log.Fatalln("Testing client by calling last block:", rpc.Path, "\n\t", err)
+
+		}
+		// If ok, set to config
+		Config.Dials[i] = rpc
+	}
+
+	// Telegram part
+	// Connect to api
+	bot, err := tgbotapi.NewBotAPI(Config.Token)
+	if err != nil {
+		log.Fatalln("Connecting to api:\n\t", err)
+	}
+	// Set updates
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	// Log
+	log.Println("Started successfully")
+	// Recieve channel updates
+	for update := range bot.GetUpdatesChan(u) {
+		// call Handler with goroutine
+		go CommandHandler(bot, update, &Config)
+	}
+}
+
+func CommandHandler(bot *tgbotapi.BotAPI, update tgbotapi.Update, config *Config_struct) {
+	var err error
+	// If recieved message commmand
+	if update.Message != nil && update.Message.IsCommand() {
+		// Switch for command
+		switch update.Message.Command() {
+
+		// Balance
+		case "balance":
+			adress := update.Message.CommandArguments()
+			// If adress is not valid
+			if !valid_adress.MatchString(adress) {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Wallet adress is *not valid*!")
+				// Set reply id
+				msg.ReplyToMessageID = update.Message.MessageID
+				// Set parse mode
+				msg.ParseMode = tgbotapi.ModeMarkdown
+				// Send message
+				_, err = bot.Send(msg)
+				if err != nil {
+					log.Println("/balance: Trying to send 'incorrect wallet' message\n\t", err)
+				}
+				return
+			}
+
+			// Get all dials
+			var (
+				// Create background context
+				background = context.Background()
+				// Wallet adress
+				hex_adress = common.HexToAddress(adress)
+				// Balance
+				balance = new(big.Int)
+				// Telegram bot output
+				output = time.Now().Format("🗓 *2006-01-02*\n⌚️ *3:04* PM\n\n")
+			)
+			// Range rpc's
+			for _, rpc := range config.Dials {
+				// Update context
+				ctx, cancel := context.WithTimeout(background, time.Duration(config.Timeout))
+				// Get balance with timeout
+				balance, err = rpc.Client.BalanceAt(ctx, hex_adress, nil)
+				if err != nil {
+					// Print Error
+					log.Printf("%s (%s):\n\t%s", rpc.Name, rpc.Path, err)
+					// Switch error
+					switch err.(type) {
+					// If server error
+					case *url.Error:
+						output += fmt.Sprintf("*%s*:  `%s`\n", rpc.Name, "_service error_")
+					}
+				}
+				// Cancel context
+				cancel()
+				// If balance not equal 0
+				if balance.String() != "0" {
+					// add to output
+					output += fmt.Sprintf("*%s*:  `%s`\n", rpc.Name, decimal.NewFromBigInt(balance, 1).Div(div_wei))
+				}
+			}
+			// New Message
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, output)
+			// Set reply id
+			msg.ReplyToMessageID = update.Message.MessageID
+			// Set parse mode
+			msg.ParseMode = tgbotapi.ModeMarkdown
+			// Send message
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Println("/balance: Trying to send reply message\n\t", err)
+			}
+
+		// Help - all commands
+		case "help":
+			// Create message
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+				`*Commands:*
+/balance: view adress eth amount
+	We check all available rpc's
+`,
+			)
+			// Set reply id
+			msg.ReplyToMessageID = update.Message.MessageID
+			// Set parse mode
+			msg.ParseMode = tgbotapi.ModeMarkdown
+			// Send message
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Println("/help: Trying to send reply message\n\t", err)
+			}
+
+		// If no one substituted:
+		default:
+			// Create message
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Command *not found!*\nType `/help` to see any")
+			// Set reply id
+			msg.ReplyToMessageID = update.Message.MessageID
+			// Set parse mode
+			msg.ParseMode = tgbotapi.ModeMarkdown
+			// Send message
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Println("Trying to send 'not found, see /help' message\n\t", err)
+			}
+		}
+	}
 }
